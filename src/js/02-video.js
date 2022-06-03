b7fad03511dd89bdf2b4cc792e42b627eacb174a
ts4ed <103256726+ts4ed@users.noreply.github.com>
2022-06-03 13:05:50 +0300

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,24 +1,22 @@
-import Player from "@vimeo/player";
-import throttle from "lodash.throttle";
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector("iframe");
+const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
-const timeOfCurrent = localStorage.getItem("videoplayer-current-time");
+const timeOfCurrent = localStorage.getItem('videoplayer-current-time');
+currentTime()
 player.on(
-  "timeupdate",
+  'timeupdate',
   throttle(function () {
     player.getCurrentTime().then(function (seconds) {
-      localStorage.setItem("videoplayer-current-time", seconds);
+      localStorage.setItem('videoplayer-current-time', seconds);
       // console.log(localStorage.setItem('videoplayer-current-time', seconds));
     });
   }, 1000)
 );
 
-player.setCurrentTime(timeOfCurrent).then(function (seconds) {
-});
-
 function currentTime() {
   if (timeOfCurrent) {
-    // console.log(timeOfCurrent);
+player.setCurrentTime(timeOfCurrent).then(function (seconds) {});
   }
 }

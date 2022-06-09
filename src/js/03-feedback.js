5e1885cce9a9abd32b5f3d66c2bdc9d34b908c52
ts4ed <103256726+ts4ed@users.noreply.github.com>
2022-06-09 16:47:09 +0300

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,11 +2,12 @@ import throttle from "lodash.throttle";
 const emailEl = document.querySelector("input[type='email']");
 const messageEl = document.querySelector("textarea");
 const formEl = document.querySelector(".feedback-form");
+const KEY = 'feedback-form-state';
 const form = {
   message: "",
   email: "",
 };
-const forms = JSON.parse(localStorage.getItem("feedback-form-state"));
+const forms = JSON.parse(localStorage.getItem(KEY));
 
 emailInput();
 messageInput();
@@ -18,32 +19,35 @@ formEl.addEventListener("submit", onFormSubmit);
 function onMessageInput(evn) {
   const message = evn.target.value;
   form.message = message;
-  localStorage.setItem("feedback-form-state", JSON.stringify(form));
+  localStorage.setItem(KEY, JSON.stringify(form));
 }
 
 function onEmailInput(evn) {
   const email = evn.target.value;
   form.email = email;
-  localStorage.setItem("feedback-form-state", JSON.stringify(form));
+  localStorage.setItem(KEY, JSON.stringify(form));
 }
 
 function emailInput() {
-  const saveEmail = localStorage.getItem("feedback-form-state");
+  const saveEmail = localStorage.getItem(KEY);
   if (saveEmail) {
     emailEl.value = forms.email;
   }
 }
 
 function messageInput() {
-  const saveMessage = localStorage.getItem("feedback-form-state");
-  if (saveMessage) {
-    messageEl.value = forms.message;
-  }
+  const saveMessage = localStorage.getItem(KEY);
+    if (saveMessage) {
+      messageEl.value = forms.message;
+    }
 }
 
 function onFormSubmit(evn) {
   evn.preventDefault();
-  console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
+  if (form.message === '' || form.email === '') {
+    alert('Не все поля заполнены!');
+    return;
+  };
   evn.target.reset();
-  localStorage.removeItem("feedback-form-state");
+  localStorage.removeItem(KEY);
 }
